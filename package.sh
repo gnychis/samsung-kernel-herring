65cf1d763750c6ef6f704c10a5a3d6ca2798f866
George Nychis <gnychis@gmail.com>
2012-02-07 08:28:26 -0800

    adding in way to package

--- package.sh
+++ package.sh
@@ -4,7 +4,7 @@ cd kernel_image
 mkdir kernel
 cp ../arch/arm/boot/zImage kernel/
 mkdir -p system/lib/modules
-find ../ -name "*.ko" -exec cp {} system/lib/modules \;
+find ../ -name "bcm*.ko" -exec cp {} system/lib/modules \;
 tar czvf ../kernel.tar *
 cd ../
 rm -fr kernel_image
